bcf3972cf7c9e6fe1a6efe8ec0d7d8af09a4570b
allozano33 <alessandro.lozano@mercadolivre.com>
2022-06-02 19:00:58 -0300

    implementando-metodos

--- src/main.kt
+++ src/main.kt
@@ -18,69 +18,80 @@ fun main() {
     println(contaAlex.numero)
     println(contaAlex.saldo)
 
-
     println("depositando na conta do Alex")
-    deposita(contaAlex, 50.0)
+    contaAlex.deposita(50.0)
     println(contaAlex.saldo)
+
     println("depositando na conta da fran")
-    deposita(contaFran, 70.0)
+    contaFran.deposita(70.0)
     println(contaFran.saldo)
-}
 
-    fun deposita(conta: Conta, valor: Double){
-        conta.saldo += valor
+    println("sacando na conta do Alex")
+    contaAlex.saca(250.0)
+    println(contaAlex.saldo)
 
+    println("sacando na conta da Fran")
+    contaFran.saca(100.0)
+    println(contaFran.saldo)
 }
 
 class Conta {
     var titular = ""
     var numero = 0
     var saldo = 0.0
-}
 
-fun testaCopiasEReferencias() {
-    val numeroX = 10
-    var numeroY = numeroX
-    numeroY++
+    fun deposita(valor: Double) {
+        this.saldo += valor
+    }
+    fun saca(valor: Double) {
+        if (this.saldo >= valor){
+            saldo -= valor
+        }
+    }
 
-    println("numeroX $numeroX")
-    println("numeroX $numeroY")
+    fun testaCopiasEReferencias() {
+        val numeroX = 10
+        var numeroY = numeroX
+        numeroY++
 
-    val contaJoao = Conta()
-    contaJoao.titular = "João"
+        println("numeroX $numeroX")
+        println("numeroX $numeroY")
 
-    val contaMaria = Conta()
-    contaMaria.titular = "Maria"
+        val contaJoao = Conta()
+        contaJoao.titular = "João"
 
-    println("titular conta joao: ${contaJoao.titular}")
-    println("titular conta maria: ${contaMaria.titular}")
+        val contaMaria = Conta()
+        contaMaria.titular = "Maria"
 
-}
+        println("titular conta joao: ${contaJoao.titular}")
+        println("titular conta maria: ${contaMaria.titular}")
 
-fun testaLacos() {
-    var i = 0
-    while (i < 5) {
-        val titular: String = "Alex $i"
-        val numeroConta: Int = 1000 + i
-        var saldo = i + 10.0
+    }
 
-        println("Titular $titular")
-        println("número da conta $numeroConta")
-        println("saldo da conta $saldo")
-        println()
+    fun testaLacos() {
+        var i = 0
+        while (i < 5) {
+            val titular: String = "Alex $i"
+            val numeroConta: Int = 1000 + i
+            var saldo = i + 10.0
+
+            println("Titular $titular")
+            println("número da conta $numeroConta")
+            println("saldo da conta $saldo")
+            println()
+        }
+    }
+
+    fun testaCondicoes(saldo: Double) {
+        if (saldo > 0.0)
+            println("conta é positiva")
+        else if (saldo == 0.0)
+            println("conta é neutra")
+        else {
+            println("conta é negativa")
+        }
     }
 }
-
-fun testaCondicoes(saldo: Double) {
-    if (saldo > 0.0)
-        println("conta é positiva")
-    else if (saldo == 0.0)
-        println("conta é neutra")
-    else {
-        println("conta é negativa")
-    }
-}
-
 
 //for (i in 5 downTo 1) {
 //    val titular: String = "Alex $i"
