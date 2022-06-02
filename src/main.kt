9f562787591c90bdc2a570db8db680eae8cca9c7
allozano33 <alessandro.lozano@mercadolivre.com>
2022-06-02 09:51:50 -0300

    Exercicios 3

--- src/main.kt
+++ src/main.kt
@@ -1,29 +1,38 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    for (i in 5 downTo 1 step 2){
+    for (i in 1..5) { //ordem crescente
+        for (i in 5 downTo 1) //ordem decrescente
+            for (i in 5 downTo 1 step 2) { //decrescente de 2 em 2
 
+                loop@ for (i in 1..100) {
+                    println("i $i")
+                    for (j in 1..100) {
+                        println("j $j")
+                        if (j == 5) break@loop
+                    }
+                }
 
-    val titular: String = "Alex $i"
-    val numeroConta: Int = 1000 + i
-    var saldo = i +10
+                val titular: String = "Alex $i"
+                val numeroConta: Int = 1000 + i
+                var saldo = i + 10.0
 
+                println("Titular $titular")
+                println("número da conta $numeroConta")
+                println("saldo da conta $saldo")
+                println()
 
-    println("Titular $titular")
-    println("número da conta $numeroConta")
-    println("saldo da conta $saldo")
+                //testaCondicoes(saldo)
+            }
 
-    //testaCondicoes(saldo)
-}
-
-fun testaCondicoes(saldo: Double) {
-    if (saldo > 0.0)
-        println("conta é positiva")
-    else if (saldo == 0.0)
-        println("conta é neutra")
-    else {
-        println("conta é negativa")
-
+        fun testaCondicoes(saldo: Double) {
+            if (saldo > 0.0)
+                println("conta é positiva")
+            else if (saldo == 0.0)
+                println("conta é neutra")
+            else {
+                println("conta é negativa")
+            }
+        }
     }
-}
 }
